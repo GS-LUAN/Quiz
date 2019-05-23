c2bea94ca3ea7d70d7b693461c4f6ad627213b4a
GS-LUAN <luansantosfc1@gmail.com>
2019-05-23 14:19:36 -0300

    fixbug

---
+++ Quiz Design/questions.js
@@ -0,0 +1,9 @@
+var questions = [
+  
+    ["(Unesp 2014)  Em um plano horizontal encontram-se representadas uma circunferência e as cordas AC e BD. Nas condições apresentadas na figura, determine o valor de x.",
+        "x=5", "x=5", "x=9", "x=2","x=7",
+        "img/ex1.JPG", "", "", "", "","",
+        "5"
+    ],
+
+]
